4928c323b1b7b62a0cdcdb8294b1d3f5405ca8a0
Abhyuday <abhyuday911@gmail.com>
2023-07-25 17:35:15 +0530

    first commit

---
+++ a.js
@@ -0,0 +1,13 @@
+let a = [1, 4, 5, 6,[1, 4, 5, 6,[1, 4, 5, 6]]];
+
+const splice = () => {
+  a.splice(1, 4, "llll");
+};
+// splice();
+// console.log(a);
+
+// console.log(a.flat(Infinity))
+
+// for (const i in a) {
+//   console.log(i);
+// }

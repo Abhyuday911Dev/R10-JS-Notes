254cff1d83d17511b1529d3fa657548df12bfcee
Abhyuday <abhyuday911@gmail.com>
2023-07-27 21:51:25 +0530

    fetch with async

--- a.js
+++ a.js
@@ -1,4 +1,4 @@
-let a = [1, 4, 5, 6,[1, 4, 5, 6,[1, 4, 5, 6]]];
+let a = [1, 4, 5, 6, [1, 4, 5, 6, [1, 4, 5, 6]]];
 
 const splice = () => {
   a.splice(1, 4, "llll");
@@ -11,3 +11,15 @@ const splice = () => {
 // for (const i in a) {
 //   console.log(i);
 // }
+
+async function GetUser() {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const user = await res.json();
+    console.log(user);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+GetUser();
